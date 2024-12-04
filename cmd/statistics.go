5c1c7957d1963983bbbde5c61500f148cd64ba3d
unknown <amarpitmalik204@gmail.com>
2024-12-04 23:11:01 +0530

    added cpu command

--- cmd/statistics.go
+++ cmd/statistics.go
@@ -13,31 +13,20 @@ const (
 	Disk   = "disk"
 )
 
-var getStatisitcs = &cobra.Command{
-	Use:   "statistics",
-	Short: "A CLI to generate machine statisitics",
-	Long: ` A CLI to get the cpu, memory and disk statisitcs"
-	For example:
-	statistics cpu percent
-	statistics memory `,
-	Run: statistics,
-}
-
-func init() {
-	rootCmd.AddCommand(getStatisitcs)
-
-	getStatisitcs.Flags().StringP("type", "t", "cpu", "Provide the type of statisitcs")
-}
-
 func statistics(cmd *cobra.Command, args []string) {
-	isType, _ := cmd.Flags().GetString("type")
+	if len(args) > 0 {
+		isType, _ := cmd.Flags().GetString("type")
 
-	if isType == CPU {
-		cpuInfo, _ := cpu.Info()
-		fmt.Println("CPU Information")
-		for _, val := range cpuInfo {
-			fmt.Println(val.ModelName)
+		if isType == CPU {
+			cpuInfo, _ := cpu.Info()
+			fmt.Println("CPU Information")
+			for _, val := range cpuInfo {
+				fmt.Println(val.ModelName)
+			}
+
 		}
-
+	} else {
+		cmd.Help()
 	}
+
 }
